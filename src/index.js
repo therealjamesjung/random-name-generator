ec738308e7c34e68388553eebfdcd704750f4802
James Jung <therealjamesjung@gmail.com>
2024-03-26 18:56:36 +0900

    ts 안써요

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as data from "./data.json";
 
-export function generateRandomNickName(): string {
+export function generateRandomNickName() {
   const { colors, foods } = data;
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   const randomFood = foods[Math.floor(Math.random() * foods.length)];
